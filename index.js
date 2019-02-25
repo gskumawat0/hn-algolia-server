6efdc34ca422b6f089d87f428710cc2f4e40f5a0
gouri shankar kumawat <gskumawat555@gmail.com>
2019-02-25 13:10:22 +0000

    remove untracked files

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-require("dotenv").config()
+require("dotenv").config();
 var express = require("express");
 var app = express();
 var cors = require('cors');
 var bodyParser = require("body-parser");
 var authRoutes = require("./routes/auth");
-var auth = require('./middleware/auth');
-var db = require("./models");
+// var auth = require('./middleware/auth');
+// var db = require("./models");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -14,11 +14,6 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.get("/", function(req,res){
   res.json({message: "Make a POST requst to /api/auth/signup to signup"});
 });
-
-// app.use('/api/users/:id/messages',
-//         auth.loginRequired, auth.ensureCorrectUser,
-//         // messagesRoutes
-//         );
         
 app.use('/api/auth', authRoutes);
 
@@ -28,6 +23,5 @@ app.listen(PORT, process.env.IP, function(){
   console.log(`Server is listening on port ${PORT}`);
 });
 
-let mongodburl = process.env.DATABASEURL;
 
 
