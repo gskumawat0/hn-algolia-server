e7f3ed3fe98e897fc80704a0308fd9086b8cc0e8
gouri shankar kumawat <gskumawat555@gmail.com>
2019-02-26 07:27:39 +0000

    authentication is working

--- helpers/auth.js
+++ helpers/auth.js
@@ -3,41 +3,42 @@ const jwt = require('jsonwebtoken');
 
 
 exports.signup = function(req, res, next) {
-  // console.log(req.body, 23131);
-  const { username, email, password } = req.body;
-  console.log(username, email, password);
-  db.User.create({ username, email, password }).then(function(user) {
-    var token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY);
-    res.status(200).json({
-      userId: user.id,
-      username: user.username,
-      token
+    // console.log(req.body, 23131);
+    const { username, email, password } = req.body;
+    console.log(username, email, password, `a signup request has arrived`);
+    db.User.create({ username, email, password }).then(function(user) {
+        var token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY);
+        res.status(200).json({
+            userId: user.id,
+            username: user.username,
+            token
+        });
+    }).catch(function(err) {
+        res.status(400).json(err);
     });
-  }).catch(function(err) {
-    res.status(400).json(err);
-  });
 };
 
 
 exports.signin = function(req, res) {
-  db.User.findOne({ email: req.body.email }).then(function(user) {
-    user.comparePassword(req.body.password, function(err, isMatch) {
-      if (isMatch) {
-        var token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY);
-        res.status(200).json({
-          userId: user.id,
-          username: user.username,
-          profileImageUrl: user.profileImageUrl,
-          token
+    console.log('a signin request has arrived');
+    db.User.findOne({ email: req.body.email }).then(function(user) {
+        user.comparePassword(req.body.password, function(err, isMatch) {
+            if (isMatch) {
+                var token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY);
+                res.status(200).json({
+                    userId: user.id,
+                    username: user.username,
+                    profileImageUrl: user.profileImageUrl,
+                    token
+                });
+            }
+            else {
+                res.status(400).json({ message: 'Invalid Email/Password.' });
+            }
         });
-      }
-      else {
-        res.status(400).json({ message: 'Invalid Email/Password.' });
-      }
+    }).catch(function(err) {
+        res.status(400).json({ message: 'Invalid Email/Password' });
     });
-  }).catch(function(err) {
-    res.status(400).json({ message: 'Invalid Email/Password' });
-  });
 };
 
 
